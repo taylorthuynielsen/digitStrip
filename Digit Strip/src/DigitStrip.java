d390f018c70e43af20d0cf0e35f873f625f31cab
taylor.nielsen <taylor.nielsen@MU-C1-22.mullen.pvt.k12.co.us>
2018-09-12 09:40:29 -0600

    second commit

--- Digit Strip/src/DigitStrip.java
+++ Digit Strip/src/DigitStrip.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class DigitStrip
@@ -6,23 +7,30 @@ public class DigitStrip
 		public static void main(String[] args)
 			{
 				//challengeOne();
-				test();
+				challengeTwo();
 			}
 		
 		public static void challengeOne()
 		{
-			int sizeSixArray[] = new int[6];
+			int array[] = new int[6];
 			Scanner userInput = new Scanner(System.in);
 			System.out.println("Input a six digit number.");
 			int number = userInput.nextInt();
+			for (int i = 0; i<array.length; i++)
+				{
+					int strippedDigit = number%10;
+					array[i] = strippedDigit;
+					number = number/10;
+				}
+				System.out.println(Arrays.toString(array));
 		}
 		
-		public static void test()
+		public static void challengeTwo()
 		{
-			int number = 237; 
-			int lastDigit = 237%10;
-			int secondDigit = (number/10)%10;
-			int firstDigit = secondDigit%10;
-			System.out.println(secondDigit);
+			Scanner userInput = new Scanner(System.in); 
+			System.out.println("Enter a number of any size.");
+			int number = userInput.nextInt(); 
+			boolean numberEqualsZero = true;
+			
 		}
 	}
