5c8134d4b4698ddbb40dda26f1814f46e3461268
taylor.nielsen <taylor.nielsen@MU-C1-22.mullen.pvt.k12.co.us>
2018-09-13 09:10:29 -0600

    challengeFive

--- Digit Strip/src/DigitStrip.java
+++ Digit Strip/src/DigitStrip.java
@@ -7,7 +7,7 @@ public class DigitStrip
 		public static void main(String[] args)
 			{
 				//challengeOne();
-				challengeTwo();
+				challengesTwoToFive();
 			}
 		
 		public static void challengeOne()
@@ -25,28 +25,60 @@ public class DigitStrip
 				System.out.println(Arrays.toString(array));
 		}
 		
-		public static void challengeTwo()
+		public static void challengesTwoToFive()
 		{
 			Scanner userInput = new Scanner(System.in); 
 			System.out.println("Enter a number of any size.");
 			int number = userInput.nextInt();
+			int numberChallengeTwo = number;
 			boolean numberEqualsZero = true;
 			int counter = 0;
 			do
 				{
-					if (number/10>0)
+					if (numberChallengeTwo/10>0)
 						{
-							int strippedDigit = number%10;
-							number = number/10; 
+							int strippedDigit = numberChallengeTwo%10;
+							numberChallengeTwo = numberChallengeTwo/10; 
 							counter++;
 					
 						}
-					else if(number/10==0)
+					else if(numberChallengeTwo/10==0)
 						{
+							counter++;
 							numberEqualsZero=false;
 						}
 				}
 			while(numberEqualsZero);
 			System.out.println(counter);
+			
+			int array[]=new int[counter];
+			for (int i = 0; i<array.length; i++)
+				{
+					int strippedDigit = number%10;
+					array[i] = strippedDigit; 
+					number = number/10;
+				}
+			System.out.println(Arrays.toString(array));
+			
+			int oddCounter = 0;
+			for (int i = 0; i<array.length; i++)
+				{
+					if ((array[i]%2)!=0)
+						{
+							oddCounter++;
+						}
+					else
+						{
+							//oddCounter = oddCounter + 0; 
+						}
+				}
+			System.out.println(oddCounter);
+			
+			int sum = 0;
+			for (int i = 0; i<array.length; i++)
+				{
+					sum = sum + array[i];
+				}
+			System.out.println(sum);
 		}
 	}
